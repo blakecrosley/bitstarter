1832bb19f88d9491992e543cdc5c2d495e62876c
Blake Crosley <blake.crosley@gmail.com>
2013-07-09 19:54:19 +0000

    Try something else.

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-    res.render('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
