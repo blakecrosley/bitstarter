002a7bd599ae3809767ef9969d55c4387e5399cf
Blake Crosley <blake.crosley@gmail.com>
2013-08-04 21:15:29 +0000

    First commit to staging branch.

--- web.js
+++ web.js
@@ -4,6 +4,9 @@ var app = express.createServer(express.logger());
 var fs = require("fs");
 
 var app = express.createServer(express.logger());
+app.configure(function(){
+  app.use('/static', express.static(__dirname + '/static'));
+});
 
 app.get('/', function(req, res) {
   var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
